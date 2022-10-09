c15aded5cca0373efc5c8eecbae3b7c27fd71ae9
Nikolay Penkov <nikolay7penkov@gmail.com>
2022-10-09 16:52:57 +0300

    this are exercises from Generic lecture

---
+++ 08. Generics/Lab/Generics Lab/GenericScale/StartUp.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GenericScale
+{
+    public class StartUp
+    {
+        public void Main(string[] args)
+        {
+            EqualityScale<int> scale = new EqualityScale<int>(5, 5);
+            scale.AreEqual();
+            Console.WriteLine(scale.AreEqual());
+        }
+    }
+}
